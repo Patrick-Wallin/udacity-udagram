a6451fc84362370bb7586c4c665e8d48825a2057
Patrick Wallin <pwmobiledeveloper@gmail.com>
2021-09-04 07:21:31 -0700

    Added eb setenv in deploy.sh.

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: "http://udacityudagramapp-env-1.eba-qcqcvppp.us-west-1.elasticbeanstalk.com/api/v0"
+    apiHost: ""
   };
